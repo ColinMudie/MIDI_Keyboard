8296993dbed03ad0f426fe64f43e1a7811d378c1
Colin Mudie <csmudie1@gmail.com>
2021-06-06 12:14:25 -0400

    added midi acces and saved the message data to the variables command, note, and velocity

--- src/App.js
+++ src/App.js
@@ -1,6 +1,53 @@
 import './App.css';
 
 function App() {
+  //checks to see if WebMIDI is supported by the current browser
+  if (navigator.requestMIDIAccess) {
+    console.log('This browser supports WebMIDI!');
+  } else {
+    console.log('WebMIDI is not supported in this browser');
+  }
+
+  navigator.requestMIDIAccess()
+    .then(onMIDISuccees, onMIDIFailure);
+
+  function onMIDISuccees(midiAccess) {
+    for (var input of midiAccess.inputs.values())
+      input.onmidimessage = getMIDIMessage;
+  }
+  function onMIDIFailure() {
+    console.log('Could not access your MIDI devices');
+  }
+
+  function getMIDIMessage(midiMessage) {
+    console.log(midiMessage);
+    var command = midiMessage.data[0];
+    var note = midiMessage.data[1];
+    var velocity = (midiMessage.data.length > 2) ? midiMessage.data[2] : 0;
+    switch (command) {
+      case 151: // noteOn
+        if (velocity > 0){
+          noteOn(note, velocity);
+        } else {
+          noteOff(note);
+        }
+        break;
+      case 135: // noteOff
+        noteOff(note);
+        break;
+      default:
+        break;
+    }
+  }
+
+  function noteOn(note, velocity) {
+    
+  }
+
+  function noteOff(note) {
+
+  }
+
   return (
     <div className="App">
       <h1>MIDI Keyboard</h1>
