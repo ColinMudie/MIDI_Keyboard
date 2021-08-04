2a6a747f9b3063fc4e2a9ed0a389c49907a0074c
Colin Mudie <csmudie1@gmail.com>
2021-08-03 20:09:52 -0400

    added list of possible minor9 chords to compare with converted notes

---
+++ src/utils/Minor9.js
@@ -0,0 +1,15 @@
+const minor9 = {
+    C: ['C ', ' G ', 'Bb ', 'D ', 'Eb ', 'G '],
+    Db: ['Db ', 'Ab ', 'B ', 'Eb ', 'E ', 'Ab '],
+    D: ['D ', 'A ', 'C ', 'E ', 'F ', 'A '],
+    Eb: ['Eb ', 'Bb ', 'Db ', 'F ', 'Gb ', 'Bb '],
+    E: ['E ', 'B ', 'D ', 'Gb ', 'G ', 'B '],
+    F: ['F ', 'C ', 'Eb ', 'G ', 'Ab ', 'C '],
+    Gb: ['Gb ', 'Db ', 'E ', 'Ab ', 'A ', 'Db '],
+    G: ['G ', 'D ', 'F ', 'A ', 'Bb ', 'D '],
+    Ab: ['Ab ', 'Eb ', 'Gb ', 'Bb ', 'B ', 'Eb '],
+    A: ['A ', 'E ', 'G ', 'B ', 'C ', 'E '],
+    Bb: ['Bb ', 'F ', 'Ab ', 'C ', 'Db ', 'F '],
+    B: ['B ', 'Gb ', 'A ', 'Db ', 'D ', 'Gb ']
+}
+export default minor9;
